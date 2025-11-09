02abda4b288b5eed570cfbe06b4a21cbaba1117b
Yehudit-code <yehudit59501@gmail.com>
2025-11-09 15:53:59 +0200

    add routes and more

---
+++ src/app/lib/db.ts
@@ -0,0 +1,23 @@
+// src/lib/db.ts
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error("Missing MONGODB_URI");
+}
+
+let cached = (global as any)._mongoose;
+if (!cached) {
+  cached = (global as any)._mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+  if (cached.conn) return cached.conn as typeof mongoose;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn as typeof mongoose;
+}
