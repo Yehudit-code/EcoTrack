b7662a16353c2e3f1b67a86b932b827dca90628a
User <hadarn26@gmail.com>
2025-11-11 14:59:27 +0200

    little fix

---
+++ src/app/signUp/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function SignUpPage() {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <p>Welcome to the registration page.</p>
+    </div>
+  );
+}
