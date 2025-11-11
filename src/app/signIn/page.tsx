c3cef954dbc08df82ad306fcda24cb755075de41
Yehudit-code <yehudit59501@gmail.com>
2025-11-11 13:00:10 +0200

    update signIn and change signUp name

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,9 +1,19 @@
-import styles from './page.module.css';
+"use client";
+
+import React from "react";
+import SignInForm from "./SignInForm";
+import styles from "./SignIn.module.css";
 
 export default function SignInPage() {
   return (
     <div className={styles.container}>
-      <h1>Sign In</h1>
+      <div className={styles.formWrapper}>
+        <h2>
+          Sign in to <span className={styles.logo}>EcoTrack</span>
+        </h2>
+
+        <SignInForm />
+      </div>
     </div>
   );
 }
