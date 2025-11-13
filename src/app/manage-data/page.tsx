888b8a7bc0ebf321639b8d91195451a604efa058
Yehudit-code <yehudit59501@gmail.com>
2025-11-13 12:05:17 +0200

    Manage date page without worked server

--- src/app/manage-data/page.tsx
+++ src/app/manage-data/page.tsx
@@ -12,6 +12,7 @@ export default function ManageData() {
   });
   const [message, setMessage] = useState("");
 
+  
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
